61d855300d0f60e67667724fab70792cff6e8170
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-06-25 16:55:25 -0500

    merge sample used for async parrallel

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { concat, interval, of } from 'rxjs';
+import { concat, interval, merge, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -12,10 +13,10 @@ export class AboutComponent implements OnInit {
 
   ngOnInit() {
 
-    const source1$ = interval(1000);
-    const source2$ = of(4, 5, 6);
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map(x => x * 10));
 
-    const result$ = concat(source1$, source2$);
+    const result$ = merge(interval1$, interval2$);
 
     result$.subscribe(console.log);
     
