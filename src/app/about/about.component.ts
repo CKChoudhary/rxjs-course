f3bb6e6e89f20c65487f7deca5dc41de93ea04e8
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-06-25 16:05:06 -0500

    concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { concat, interval, of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,15 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const source1$ = interval(1000);
+    const source2$ = of(4, 5, 6);
+
+    const result$ = concat(source1$, source2$);
+
+    result$.subscribe(console.log);
+    
+
   }
 
 }
