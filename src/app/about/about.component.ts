b58780ec77f950cb4d284b68b527f507971860a1
Chanchal Choudhary <Chanchal.Choudhary@fcsamerica.com>
2022-06-25 11:09:13 -0500

    callback hell

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,40 +1,43 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import {
-    concat,
-    fromEvent,
-    interval,
-    noop,
-    observable,
-    Observable,
-    of,
-    timer,
-    merge,
-    Subject,
-    BehaviorSubject,
-    AsyncSubject,
-    ReplaySubject
-} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+  concat,
+  fromEvent,
+  interval,
+  noop,
+  observable,
+  Observable,
+  of,
+  timer,
+  merge,
+  Subject,
+  BehaviorSubject,
+  AsyncSubject,
+  ReplaySubject,
+} from "rxjs";
+import { delayWhen, filter, map, take, timeout } from "rxjs/operators";
+import { createHttpObservable } from "../common/util";
 
 @Component({
-    selector: 'about',
-    templateUrl: './about.component.html',
-    styleUrls: ['./about.component.css']
+  selector: "about",
+  templateUrl: "./about.component.html",
+  styleUrls: ["./about.component.css"],
 })
 export class AboutComponent implements OnInit {
+  constructor() {}
 
-    ngOnInit() {
+  ngOnInit() {
+    document.addEventListener("click", (clickEvent: MouseEvent) => {
+      console.log("Click Event Details: ", clickEvent);
 
-
-    }
-
-
+      setTimeout(() => {
+        console.log("finished");
+        let counter = 0;
+        setInterval(() => {
+          console.log(counter);
+          counter++;
+        }, 1000);
+          
+      }, 3000);
+    });
+  }
 }
-
-
-
-
-
-
